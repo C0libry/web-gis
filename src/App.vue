1d1c6e5587b3590a2ad93db9c385446c94be1a77
C0libry <pe.pe.l@yandex.ru>
2024-01-02 14:49:28 +0300

    Start web-gis project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Map from './components/Map.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Map msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Map.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="map" id="map">
+  </div>
+</template>
+
+<style scoped></style>
